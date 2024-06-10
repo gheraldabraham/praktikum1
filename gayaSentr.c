c77bb23ad41f72bd9ba85044e26bf3045accdc3e
gheraldabraham <gherald061204@gmail.com>
2024-06-10 18:58:10 +0700

    Create gayaSentr.c

---
+++ gayaSentr.c
@@ -0,0 +1,31 @@
+/* Nama         : Gherald Abraham
+   NIM          : 24060123140210
+   Kelas        : C
+   Nama Program : gayaSentr.c
+   Deskripsi    : Menghitung dan menampilkan hasil dari Gaya Sentral
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+    //Kamus
+    int m,v,r;
+    float F;
+
+    //Algoritma
+    printf("Masukkan : \n");
+    printf("m = ");
+    scanf("%d",&m);
+    printf("v = ");
+    scanf("%d",&v);
+    printf("r = ");
+    scanf("%d",&r);
+
+    F = m*(v*v/r);
+    printf("F = %.2f \n", F);
+    printf("Jadi gaya sentripetalnya adalah %.2f",F);
+
+    return 0;
+}
